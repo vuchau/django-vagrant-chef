3d13ff501eb681d9b2ce9c7607ff1e1a075d5dcd
Manuel Ignacio Franco <maigfrga@gmail.com>
2014-08-11 23:50:15 +0100

    change supervisor version

--- roles/django.rb
+++ roles/django.rb
@@ -1,6 +1,6 @@
 name 'django'
 description 'Django workstation Role'
-run_list('recipe[base]', 'recipe[postgresql::server]',  'recipe[postgresql::client]' ,  'recipe[supervisor]', 'recipe[python]',  'recipe[django]')
+run_list('recipe[base]', 'recipe[postgresql::server]',  'recipe[postgresql::client]' , 'recipe[python]', 'recipe[supervisor]',  'recipe[django]')
 
 default_attributes()
 override_attributes()
--- site-cookbooks/django/recipes/python.rb
+++ site-cookbooks/django/recipes/python.rb
@@ -1,5 +1,12 @@
 include_recipe 'python'
 
+%w{libcrack2-dev libpq-dev libcurl4-openssl-dev python-dev libevent-dev}.each do |pkg|
+  package pkg do
+      action :install
+  end
+end
+
+
 directory node['python']['venv_dir'] do
   mode '0775'
   owner 'root'
@@ -14,24 +21,13 @@ python_virtualenv node['python']['venv_dir'] do
   action :create
 end
 
-%w{libcrack2-dev libpq-dev libcurl4-openssl-dev python-dev libevent-dev}.each do |pkg|
-  package pkg do
-      action :install
-  end
-end
 
 node['django']['python']['packages'].each do |pkg, version|
-  python_pip pkg.to_s do
-      version version
-      virtualenv node['python']['venv_dir']
-   end
+python_pip pkg.to_s do
+    version version
+    virtualenv node['python']['venv_dir']
+ end
 end
 
 
-python_pip "Distribute" do
-  action :install
-  end
 
-python_pip "supervisor" do
-    action :upgrade
-end
